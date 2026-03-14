6f3d55730340c7b7dd19097d7f9b36a8e2958a05
Edouard3dee <pedouard@byupathway.edu>
2026-03-14 06:19:36 -0400

    web developement firt work

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = `©️${currentYear} Peter Edouard Santiago, Dominican Republic`;
+
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
